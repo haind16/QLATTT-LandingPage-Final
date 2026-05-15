8c47201e02407d766c3b3e52bc79377a52200c35
ndhai1610 <167769102+haind16@users.noreply.github.com>
2026-05-15 23:42:41 +0700

    commit

--- QLATTT-LandingPage-BE-main/src/db.js
+++ QLATTT-LandingPage-BE-main/src/db.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // hoặc 'mysql' tùy thư viện bạn dùng
 
-const db = mysql.createConnection({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Bắt buộc phải có dòng này để qua mặt lỗi chứng chỉ SSL
+  }
 });
+
 
 db.connect(err => {
   if (err) {
